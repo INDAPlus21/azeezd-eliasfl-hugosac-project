b4debe1444ea2146cfb4da41c12e3b470b296127
AzeezDaoud <azeez.daoud3@gmail.com>
2022-05-12 11:06:29 +0200

    Make collision handler a bit more readable

--- src/game/movement/collision_handler.rs
+++ src/game/movement/collision_handler.rs
@@ -9,23 +9,26 @@ pub struct CollisionHandler {
 impl CollisionHandler {
     pub fn new(current: [f32; 3], new_pos: [f32; 3], block: [f32; 3]) -> Self {
         
+        // Current position in block, 1=positive side 2=negative side
         let x1 = current[0] - PLAYER_SIZE_FROM_CENTER < block[0] + BLOCK_SIZE_FROM_CENTER;
         let x2 = current[0] + PLAYER_SIZE_FROM_CENTER > block[0] - BLOCK_SIZE_FROM_CENTER;
-        let y1 = block[1] + BLOCK_SIZE_FROM_CENTER > current[1] - HEIGHT + BLOCK_SIZE_FROM_CENTER;
-        let y2 = block[1] - BLOCK_SIZE_FROM_CENTER < current[1] + HEAD_HEIGHT;
+        let y1 = block[1] + BLOCK_SIZE_FROM_CENTER > current[1] - HEIGHT + BLOCK_SIZE_FROM_CENTER; // Touch at feet
+        let y2 = block[1] - BLOCK_SIZE_FROM_CENTER < current[1] + HEAD_HEIGHT; // Touch at top of head (has a height)
         let z1 = current[2] - PLAYER_SIZE_FROM_CENTER < block[2] + BLOCK_SIZE_FROM_CENTER;
         let z2 = current[2] + PLAYER_SIZE_FROM_CENTER > block[2] - BLOCK_SIZE_FROM_CENTER;
+
+        // New position will touch block 1=positive, 2=negative
         let xn1 = new_pos[0] - PLAYER_SIZE_FROM_CENTER < block[0] + BLOCK_SIZE_FROM_CENTER;
         let xn2 = new_pos[0] + PLAYER_SIZE_FROM_CENTER > block[0] - BLOCK_SIZE_FROM_CENTER;
-        let yn1 = block[1] + BLOCK_SIZE_FROM_CENTER > new_pos[1] - HEIGHT + BLOCK_SIZE_FROM_CENTER;
-        let yn2 = block[1] - BLOCK_SIZE_FROM_CENTER < new_pos[1] + HEAD_HEIGHT;
+        let yn1 = block[1] + BLOCK_SIZE_FROM_CENTER > new_pos[1] - HEIGHT + BLOCK_SIZE_FROM_CENTER; // Touch at feet
+        let yn2 = block[1] - BLOCK_SIZE_FROM_CENTER < new_pos[1] + HEAD_HEIGHT; // Touch at top of head (has a height)
         let zn1 = new_pos[2] - PLAYER_SIZE_FROM_CENTER < block[2] + BLOCK_SIZE_FROM_CENTER;
         let zn2 = new_pos[2] + PLAYER_SIZE_FROM_CENTER > block[2] - BLOCK_SIZE_FROM_CENTER;
         
         Self {
-            x_collision: y1 && y2 && ((xn1 && x2) || (x1 && xn2)) && z1 && z2,
-            y_collision: x1 && x2 && z1 && z2 && ((yn1 && y2) || (y1 && yn2)),
-            z_collision: y1 && y2 && x1 && x2 && ((zn1 && z2) || (z1 && zn2)),
+            x_collision: ((xn1 && x2) || (x1 && xn2)) && y1 && y2 && z1 && z2, // Touch happens in x
+            y_collision: x1 && x2 && ((yn1 && y2) || (y1 && yn2))&& z1 && z2, // Touch happens in y
+            z_collision: x1 && x2 && y1 && y2  && ((zn1 && z2) || (z1 && zn2)), // Touch happens in z
         }
     }
 }

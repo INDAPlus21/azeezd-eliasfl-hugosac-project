585b805ff679f7ce80f244fb34ac6f6b3ecc09b6
Hugo Sacilotto <hugge.saci@gmail.com>
2022-05-19 21:31:33 +0200

    Cool movement

--- src/game/movement/collision_handler.rs
+++ src/game/movement/collision_handler.rs
@@ -1,34 +1,28 @@
-use crate::game::{BLOCK_SIZE_FROM_CENTER, HEIGHT, PLAYER_SIZE_FROM_CENTER, HEAD_HEIGHT};
+use crate::game::{BLOCK_SIZE_FROM_CENTER, HEAD_HEIGHT, HEIGHT, PLAYER_SIZE_FROM_CENTER};
 
-pub struct CollisionHandler {
-    pub x_collision: bool,
-    pub y_collision: bool,
-    pub z_collision: bool
-}
+pub fn collision(current: [f32; 3], new_pos: [f32; 3], block: [f32; 3]) -> bool {
+    // Current position in block, 1=positive side 2=negative side
+    let x1 = current[0] - PLAYER_SIZE_FROM_CENTER < block[0] + BLOCK_SIZE_FROM_CENTER;
+    let x2 = current[0] + PLAYER_SIZE_FROM_CENTER > block[0] - BLOCK_SIZE_FROM_CENTER;
 
-impl CollisionHandler {
-    pub fn new(current: [f32; 3], new_pos: [f32; 3], block: [f32; 3]) -> Self {
-        
-        // Current position in block, 1=positive side 2=negative side
-        let x1 = current[0] - PLAYER_SIZE_FROM_CENTER < block[0] + BLOCK_SIZE_FROM_CENTER;
-        let x2 = current[0] + PLAYER_SIZE_FROM_CENTER > block[0] - BLOCK_SIZE_FROM_CENTER;
-        let y1 = block[1] + BLOCK_SIZE_FROM_CENTER > current[1] - HEIGHT + BLOCK_SIZE_FROM_CENTER; // Touch at feet
-        let y2 = block[1] - BLOCK_SIZE_FROM_CENTER < current[1] + HEAD_HEIGHT; // Touch at top of head (has a height)
-        let z1 = current[2] - PLAYER_SIZE_FROM_CENTER < block[2] + BLOCK_SIZE_FROM_CENTER;
-        let z2 = current[2] + PLAYER_SIZE_FROM_CENTER > block[2] - BLOCK_SIZE_FROM_CENTER;
+    let y1 = block[1] + BLOCK_SIZE_FROM_CENTER > current[1] - HEIGHT; // Touch at feet
+    let y2 = block[1] - BLOCK_SIZE_FROM_CENTER < current[1] + HEAD_HEIGHT; // Touch at top of head (has a height)
 
-        // New position will touch block 1=positive, 2=negative
-        let xn1 = new_pos[0] - PLAYER_SIZE_FROM_CENTER < block[0] + BLOCK_SIZE_FROM_CENTER;
-        let xn2 = new_pos[0] + PLAYER_SIZE_FROM_CENTER > block[0] - BLOCK_SIZE_FROM_CENTER;
-        let yn1 = block[1] + BLOCK_SIZE_FROM_CENTER > new_pos[1] - HEIGHT + BLOCK_SIZE_FROM_CENTER; // Touch at feet
-        let yn2 = block[1] - BLOCK_SIZE_FROM_CENTER < new_pos[1] + HEAD_HEIGHT; // Touch at top of head (has a height)
-        let zn1 = new_pos[2] - PLAYER_SIZE_FROM_CENTER < block[2] + BLOCK_SIZE_FROM_CENTER;
-        let zn2 = new_pos[2] + PLAYER_SIZE_FROM_CENTER > block[2] - BLOCK_SIZE_FROM_CENTER;
-        
-        Self {
-            x_collision: ((xn1 && x2) || (x1 && xn2)) && y1 && y2 && z1 && z2, // Touch happens in x
-            y_collision: x1 && x2 && ((yn1 && y2) || (y1 && yn2))&& z1 && z2, // Touch happens in y
-            z_collision: x1 && x2 && y1 && y2  && ((zn1 && z2) || (z1 && zn2)), // Touch happens in z
-        }
-    }
+    let z1 = current[2] - PLAYER_SIZE_FROM_CENTER < block[2] + BLOCK_SIZE_FROM_CENTER;
+    let z2 = current[2] + PLAYER_SIZE_FROM_CENTER > block[2] - BLOCK_SIZE_FROM_CENTER;
+
+    // New position will touch block 1=positive, 2=negative
+    let xn1 = new_pos[0] - PLAYER_SIZE_FROM_CENTER < block[0] + BLOCK_SIZE_FROM_CENTER;
+    let xn2 = new_pos[0] + PLAYER_SIZE_FROM_CENTER > block[0] - BLOCK_SIZE_FROM_CENTER;
+
+    let yn1 = block[1] + BLOCK_SIZE_FROM_CENTER > new_pos[1] - HEIGHT; // Touch at feet
+    let yn2 = block[1] - BLOCK_SIZE_FROM_CENTER < new_pos[1] + HEAD_HEIGHT; // Touch at top of head (has a height)
+
+    let zn1 = new_pos[2] - PLAYER_SIZE_FROM_CENTER < block[2] + BLOCK_SIZE_FROM_CENTER;
+    let zn2 = new_pos[2] + PLAYER_SIZE_FROM_CENTER > block[2] - BLOCK_SIZE_FROM_CENTER;
+
+    return (((xn1 && x2) || (x1 && xn2)) && y1 && y2 && z1 && z2) // Touch happens in x
+        || (x1 && x2 && ((yn1 && y2) || (y1 && yn2))&& z1 && z2) // Touch happens in y
+        || (x1 && x2 && y1 && y2  && ((zn1 && z2) || (z1 && zn2))) // Touch happens in z
+    ;
 }
--- src/game/movement/movement.rs
+++ src/game/movement/movement.rs
@@ -6,8 +6,10 @@ use amethyst::{
     input::{InputHandler, StringBindings},
 };
 
-use super::CollisionHandler;
-use crate::game::{Block, Player};
+use super::collision;
+use crate::game::{
+    Block, Player, BLOCK_SIZE_FROM_CENTER, HEAD_HEIGHT, HEIGHT, PLAYER_SIZE_FROM_CENTER,
+};
 
 use std::f32::consts::FRAC_1_SQRT_2;
 
@@ -43,67 +45,161 @@ impl<'s> System<'s> for MovementSystem {
                 self.speed * dt
             };
 
+            // Transformation for new position
             let mut transf = local.clone();
-            let current = local.translation();
+
+            // x movement and collision
+            if let Some(movement) = x_mov {
+                let current = transf.translation().clone();
+
+                // Find matrix of new position
+                transf.append_rotation_x_axis(-player.vert_rotation);
+
+                transf.append_translation_xyz(movement * dv, 0., 0.);
+
+                // Rotate back
+                transf.append_rotation_x_axis(player.vert_rotation);
+
+                let x_transf = transf.translation().clone();
+
+                let mut new_pos: [f32; 3] = [x_transf[0], x_transf[1], x_transf[2]];
+
+                // Check for collisions with blocks
+                for block in (&blocks).join() {
+                    if collision(
+                        [current[0], current[1], current[2]],
+                        [x_transf[0], x_transf[1], x_transf[2]],
+                        block.as_array(),
+                    ) {
+                        // Positive if moving forward, negative if moving backwards
+                        if movement > 0. {
+                            // Place on correct side of block
+                            new_pos[0] = block.x - BLOCK_SIZE_FROM_CENTER - PLAYER_SIZE_FROM_CENTER;
+                        } else {
+                            new_pos[0] = block.x + BLOCK_SIZE_FROM_CENTER + PLAYER_SIZE_FROM_CENTER;
+                        }
+
+                        // Player can't collide with multiple blocks at the same time
+                        break;
+                    }
+                }
+
+                // Move to new position
+                let delta_x = new_pos[1] - [current[0], current[1], current[2]][0];
+
+                transf.append_translation_xyz(delta_x, 0., 0.);
+            }
+
+            // z movement and collision
+            if let Some(movement) = z_mov {
+                let current = transf.translation().clone();
+
+                // Find matrix of new position
+                transf.append_rotation_x_axis(-player.vert_rotation);
+
+                transf.append_translation_xyz(0., 0., movement * dv);
+
+                // Rotate back
+                transf.append_rotation_x_axis(player.vert_rotation);
+
+                let z_transf = transf.translation().clone();
+
+                let mut new_pos: [f32; 3] = [z_transf[0], z_transf[1], z_transf[2]];
+
+                // Check for collisions with blocks
+                for block in (&blocks).join() {
+                    if collision(
+                        [current[0], current[1], current[2]],
+                        [z_transf[0], z_transf[1], z_transf[2]],
+                        block.as_array(),
+                    ) {
+                        // Positive if moving forward, negative if moving backwards
+                        if movement > 0. {
+                            // Place on correct side of block
+                            new_pos[2] = block.z - BLOCK_SIZE_FROM_CENTER - PLAYER_SIZE_FROM_CENTER;
+                        } else {
+                            new_pos[2] = block.z + BLOCK_SIZE_FROM_CENTER + PLAYER_SIZE_FROM_CENTER;
+                        }
+
+                        // Player can't collide with multiple blocks at the same time
+                        break;
+                    }
+                }
+
+                // Move to new position
+
+                // The movement in the z direction
+                let delta_z = new_pos[2] - [current[0], current[1], current[2]][2];
+
+                // Move to new position
+                transf.append_translation_xyz(0., 0., delta_z);
+            }
 
             // Find matrix of new position
             transf.append_rotation_x_axis(-player.vert_rotation);
-            if let Some(movement) = x_mov {
-                transf.append_translation_xyz(movement * dv, 0., 0.);
-            }
-            if let Some(movement) = z_mov {
-                transf.append_translation_xyz(0., 0., movement * dv);
-            }
+
+            // y movement and collision
             if let Some(movement) = y_mov {
                 if player.can_jump && movement > 0. {
                     player.y_velocity += 5. * movement;
                 }
-            }
-            // Calculate gravity
-            let v = player.y_velocity;
-            let dy = v * dt + GRAVITY * dt * dt; // dy = v dt + g dt^2
-            let mut v_new = (v + GRAVITY * dt).max(TERMINAL_VELOCITY); // v = v0 + g dt
-            transf.append_translation_xyz(0.0, dy, 0.0);
-            transf.append_rotation_x_axis(player.vert_rotation);
 
-            // Find change
-            let mut delta: [f32; 3] = (transf.translation() - local.translation()).into();
-            let transf = transf.translation();
+                let current = transf.translation().clone();
 
-            player.can_jump = false;
+                // Calculate gravity
+                let v = player.y_velocity;
+                let dy = v * dt + GRAVITY * dt * dt; // dy = v dt + g dt^2
+                let mut v_new = (v + GRAVITY * dt).max(TERMINAL_VELOCITY); // v = v0 + g dt
+                transf.append_translation_xyz(0.0, dy, 0.0);
 
-            // Check collision with blocks
-            for block in (&blocks).join() {
-                let collision = CollisionHandler::new(
-                    [current[0], current[1], current[2]],
-                    [transf[0], transf[1], transf[2]],
-                    block.as_array(),
-                );
+                player.can_jump = false;
 
-                // Update the deltas based on collision in each axis
-                if collision.x_collision {
-                    delta[0] = 0.0
-                }
-                if collision.z_collision {
-                    delta[2] = 0.0
-                }
-                if collision.y_collision {
-                    if delta[1] <= 0. { // Colliding from top
-                        player.can_jump = true;
-                        delta[1] = 0.0;
-                        v_new = 0.0;
-                    } else { // Colliding from bottom
-                        delta[1] -= delta[1];
-                        v_new = 0.0;
+                // Rotate back
+                transf.append_rotation_x_axis(player.vert_rotation);
+
+                let y_transf = transf.translation().clone();
+
+                let mut new_pos: [f32; 3] = [y_transf[0], y_transf[1], y_transf[2]];
+
+                // Check for collisions with blocks
+                for block in (&blocks).join() {
+                    if collision(
+                        [current[0], current[1], current[2]],
+                        [y_transf[0], y_transf[1], y_transf[2]],
+                        block.as_array(),
+                    ) {
+                        // Move player in y direction up to bottom of block
+                        new_pos[1] = block.y - BLOCK_SIZE_FROM_CENTER - HEAD_HEIGHT;
+
+                        // Positive if moving upwards, negative if moving down
+                        if dy < 0. {
+                            // moving down
+                            // Place on correct side of block
+                            new_pos[1] = block.y + BLOCK_SIZE_FROM_CENTER + HEIGHT;
+
+                            player.can_jump = true;
+                        }
+
+                        v_new = 0.;
+
+                        // Player can't collide with multiple blocks at the same time
+                        break;
                     }
                 }
+
+                // The movement in the y direction
+                let delta_y = new_pos[2] - [current[0], current[1], current[2]][2];
+
+                // Move to new position
+                transf.append_translation_xyz(0., delta_y, 0.);
+
+                player.y_velocity = v_new;
             }
 
-            // Change position based on deltas
+            let delta: [f32; 3] = (transf.translation() - local.translation()).into();
             local.prepend_translation_x(delta[0]);
             local.prepend_translation_y(delta[1]);
             local.prepend_translation_z(delta[2]);
-            player.y_velocity = v_new;
         }
     }
 }
